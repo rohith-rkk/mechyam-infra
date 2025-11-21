b580907852c59031da08f5e5c976c648cee5b4d8
Rohith Kumar <rohithkumarparamandla@gmail.com>
2025-11-21 13:16:35 +0530

    Enable public IP mapping for VPC instances
    Updated VPC module configuration to enable public IP mapping on launch.

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,6 @@ terraform {
 provider "aws" {
   region = var.region
 }
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
@@ -21,11 +20,10 @@ module "vpc" {
 
   azs             = ["${var.region}a", "${var.region}b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
   enable_nat_gateway = false
+  map_public_ip_on_launch = true
 }
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
